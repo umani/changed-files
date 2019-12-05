de65dd787f509d089e7ed276e76313f6aade75aa
Duarte Nunes <duarte.m.nunes@gmail.com>
2019-12-05 13:19:57 -0300

    Use eslint instead of standard
    Signed-off-by: Duarte Nunes <duarte.m.nunes@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,62 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import * as core from "@actions/core"
+import * as github from "@actions/github"
 
 class ChangedFiles {
-  updated: Array<string> = []
-  created: Array<string> = []
-  deleted: Array<string> = []
+    updated: Array<string> = []
+    created: Array<string> = []
+    deleted: Array<string> = []
 }
 
-async function getChangedFiles(
-  client: github.GitHub,
-  prNumber: number
-): Promise<ChangedFiles> {
-  const listFilesResponse = await client.pulls.listFiles({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    pull_number: prNumber
-  })
+async function getChangedFiles(client: github.GitHub, prNumber: number): Promise<ChangedFiles> {
+    const listFilesResponse = await client.pulls.listFiles({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: prNumber,
+    })
 
-  console.log('Found changed files:')
-  return listFilesResponse.data.reduce((acc: ChangedFiles, f) => {
-    console.log(f)
-    if (f.status === 'added') {
-      acc.created.push(f.filename)
-    } else if (f.status === 'removed') {
-      acc.deleted.push(f.filename)
-    } else if (f.status === 'modified') {
-      acc.updated.push(f.filename)
-    } else if (f.status === 'renamed') {
-      acc.created.push(f.filename)
-      acc.deleted.push(f['previous_filename'])
-    }
-    return acc
-  }, new ChangedFiles())
+    console.log("Found changed files:")
+    return listFilesResponse.data.reduce((acc: ChangedFiles, f) => {
+        console.log(f)
+        if (f.status === "added") {
+            acc.created.push(f.filename)
+        } else if (f.status === "removed") {
+            acc.deleted.push(f.filename)
+        } else if (f.status === "modified") {
+            acc.updated.push(f.filename)
+        } else if (f.status === "renamed") {
+            acc.created.push(f.filename)
+            acc.deleted.push(f["previous_filename"])
+        }
+        return acc
+    }, new ChangedFiles())
 }
 
 function getPrNumber(): number | null {
-  const pullRequest = github.context.payload.pull_request
-  return pullRequest ? pullRequest.number : null
+    const pullRequest = github.context.payload.pull_request
+    return pullRequest ? pullRequest.number : null
 }
 
-async function run() {
-  try {
-    const token = core.getInput('repo-token', { required: true })
-    const client = new github.GitHub(token)
+async function run(): Promise<void> {
+    try {
+        const token = core.getInput("repo-token", { required: true })
+        const client = new github.GitHub(token)
 
-    const prNumber = getPrNumber()
-    if (prNumber == null) {
-      core.setFailed('Could not get pull request number from context, exiting')
-      return
+        const prNumber = getPrNumber()
+        if (prNumber == null) {
+            core.setFailed("Could not get pull request number from context, exiting")
+            return
+        }
+
+        core.debug(`Fetching changed files for pr #${prNumber}`)
+        const changedFiles = await getChangedFiles(client, prNumber)
+
+        core.exportVariable("FILES_CREATED", changedFiles.created.join(" "))
+        core.exportVariable("FILES_UPDATED", changedFiles.updated.join(" "))
+        core.exportVariable("FILES_DELETED", changedFiles.deleted.join(" "))
+    } catch (error) {
+        core.error(error)
+        core.setFailed(error.message)
     }
-
-    core.debug(`Fetching changed files for pr #${prNumber}`)
-    const changedFiles = await getChangedFiles(client, prNumber)
-
-    core.exportVariable('FILES_CREATED', changedFiles.created.join(" "))
-    core.exportVariable('FILES_UPDATED', changedFiles.updated.join(" "))
-    core.exportVariable('FILES_DELETED', changedFiles.deleted.join(" "))
-  } catch (error) {
-    core.error(error)
-    core.setFailed(error.message)
-  }
 }
 
 run()

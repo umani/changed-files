995663a83d0ca7d88d125e386e57807bcd10adc0
Kevin Pruett <pruett.kevin@gmail.com>
2020-04-19 16:15:10 -0400

    Add optional `pr-number` input (#8)
    This optional input allows users to run this action outside of the pull_request event context.
    
    Certain pipelines are not conducive to running solely inside the pull_request event. For example, we are creating a custom deployment event triggered by a Netlify webhook when the build succeeds on their end. After this event triggers, we'd then like to use the changed files of the PR alongside the deployed preview url.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
+import { GetResponseDataTypeFromEndpointMethod } from "@octokit/types"
+import { Octokit } from "@octokit/rest"
+import { WebhookPayload } from "@actions/github/lib/interfaces"
+
+const octokit = new Octokit()
+type GetPullResponseDataType = GetResponseDataTypeFromEndpointMethod<typeof octokit.pulls.get>
 
 class ChangedFiles {
     updated: Array<string> = []
@@ -44,15 +50,31 @@ async function getChangedFiles(client: github.GitHub, prNumber: number, fileCoun
     }
     return changedFiles
 }
+async function fetchPr(client: github.GitHub): Promise<GetPullResponseDataType | WebhookPayload["pull_request"]> {
+    const prNumberInput = core.getInput("pr-number")
+
+    // If user provides pull request number, we fetch and return that particular pull request
+    if (prNumberInput) {
+        const { data: pr } = await client.pulls.get({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: parseInt(prNumberInput, 10),
+        })
+        return pr
+    }
+
+    // Otherwise, we infer the pull request based on the the event's context
+    return github.context.payload.pull_request
+}
 
 async function run(): Promise<void> {
     try {
         const token = core.getInput("repo-token", { required: true })
         const client = new github.GitHub(token)
+        const pr = await fetchPr(client)
 
-        const pr = github.context.payload.pull_request
         if (!pr) {
-            core.setFailed("Could not get pull request number from context, exiting")
+            core.setFailed(`Could not get pull request from context, exiting`)
             return
         }
 

d23c84bcafea22dbe89ba7897b040206274ce404
Marco Lecheler <marco.lecheler@mercedes-benz.com>
2022-07-04 08:33:27 +0200

    feat: add changed output

--- src/main.ts
+++ src/main.ts
@@ -107,6 +107,14 @@ async function run(): Promise<void> {
 
     const encoder = getEncoder();
 
+    let hasChanged = false;
+    const totalModified =
+        changedFiles.created.length + changedFiles.updated.length + changedFiles.deleted.length;
+    if (totalModified > 0) {
+        hasChanged = true;
+    }
+
+    core.setOutput("changed", String(hasChanged));
     core.setOutput("files_created", encoder(changedFiles.created));
     core.setOutput("files_updated", encoder(changedFiles.updated));
     core.setOutput("files_deleted", encoder(changedFiles.deleted));
